f94ae739cbf72645dd8a9a1a599867e04c3a1927
912187345 <912187345@qq.com>
2018-05-23 16:18:08 +0800

    抽象tips

---
+++ src/components/noListTips.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="tips">
+        <img :src="imgSrc" v-if='imgSrc' :alt="imgSrc">
+        {{ content }}
+    </div>
+</template>
+
+<script>
+export default {
+    props:['content','imgSrc']
+}
+</script>
+
+<style lang='scss' scoped>
+.tips{
+        background: #ffffff;
+        text-align: center;
+        font-size: 20px;
+        display: flex;
+        justify-content:center;
+        align-items: center;
+        height: 400px;
+        >img{
+            width: 60px;
+            height: 60px;
+            margin-right: 16px;
+        }
+    }
+</style>
